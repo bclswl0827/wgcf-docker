b3037bda5c80fadd1828311f512caa4ffefe10bd
Yuki Kikuchi <24319042+bclswl0827@users.noreply.github.com>
2022-05-22 15:07:23 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@
 /sbin/ip -4 rule add from $(ip route get 1.1.1.1 2>/dev/null | grep -oP 'src \K\S+') lookup main prio 18
 # Start WireGuard
 mkdir -p /etc/wireguard
-chmod 600 /etc/wireguard
 /usr/bin/wg-quick up wgcf
 # Force change DNS
 echo "nameserver 8.8.8.8" | tee /etc/resolv.conf
