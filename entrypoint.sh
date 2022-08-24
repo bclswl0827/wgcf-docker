20435b5984f16ac4774aa1212da5f2a47a483cff
Yuki Kikuchi <24319042+bclswl0827@users.noreply.github.com>
2022-08-24 13:59:03 +0800

    Keep VPN connection

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,14 @@ mkdir -p /etc/wireguard
 /usr/bin/wg-quick up wgcf
 # Force change DNS
 echo "nameserver 8.8.8.8" | tee /etc/resolv.conf
+# Keep VPN connection
+while(true); do
+    sleep 30
+    /usr/bin/curl --connect-timeout 5 \
+        --silent https://1.0.0.1/cdn-cgi/trace
+    if [ $? != 0 ]; then
+        /usr/bin/killall socks5
+    fi
+done &
 # Start proxy server
 /usr/local/bin/socks5
